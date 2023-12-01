9f3b0eff8a95324b84ae2cb0031115c71f44c915
devPrasanthKumar <prok52010get@gmail.com>
2023-11-30 22:14:42 -0800

    new

--- Proj/settings.py
+++ Proj/settings.py
@@ -20,11 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = os.getenv('secret')
-SECRET_KEY = "9085e4b455067b582852eb9ef8fcfe54"
+SECRET_KEY = os.getenv('secret')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
